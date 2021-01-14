ac7cf126ec31332ff2800df53060935a5f319252
ZhiqiangGuo1994 <1036105855@qq.com>
2021-01-14 17:31:46 +0800

    简单修改

--- src/main/java/com/springboot/sampleWebApp/SampleWebAppApplication.java
+++ src/main/java/com/springboot/sampleWebApp/SampleWebAppApplication.java
@@ -3,6 +3,9 @@ package com.springboot.sampleWebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 10361
+ */
 @SpringBootApplication
 public class SampleWebAppApplication {
 
